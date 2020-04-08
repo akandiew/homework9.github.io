ce0afd908936e950e59864df724476485b26b457
akandiew <andrewkandiew@Andrews-MBP.home>
2020-04-07 22:08:23 -0400

    command line inquiry

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,39 @@
-const questions = [
+// const questions = [
 
-];
+// ];
 
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
+
+// Initiate console request for GitHub repo
+const inquirer = require("inquirer");
+const fs = require("fs");
+getData();
+
+async function getData() {
+    try {
+        const answers = await inquirer.prompt([
+            {
+            message: "What is your GitHub username? ",
+            name: "name"
+        },
+            {
+            message: "Which repository are you looking for? ",
+            name: "repo"
+
+        },
+        ]);
+        console.log(answers);
+        
+    } catch (err) {
+        console.log(err);
+    }
+
 }
 
-function init() {
-
-}
-
-init();
