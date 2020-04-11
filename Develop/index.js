38562683d2e6393a819eb0d9e994fe2bd37c9835
akandiew <andrewkandiew@Andrews-MBP.home>
2020-04-11 07:28:47 -0400

    write answers to HTML / 1st cut

--- Develop/index.js
+++ Develop/index.js
@@ -26,7 +26,7 @@ async function getData() {
         const answers = await inquirer.prompt([
             {
                 message: "What is your GitHub username? ",
-                name: "name"
+                name: "username"
             },
             {
                 message: "Which project title are you looking for? ",
@@ -94,4 +94,54 @@ async   function    getGHdata({name}) {
 
 }
 // build READ.ME template // check out last activity for ideas
-
+function generateHTML(answers) {
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${answers.username}</h1>
+      <p class="lead">I am from ${answers.location}.</p>
+      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${answers.github}</li>
+        <li class="list-group-item">My project title is: ${answers.pTitle}</li>
+        <li class="list-group-item">Description:  ${answers.pDescription}</li>
+        <li class="list-group-item">Here is the project's Table of Contents:  ${answers.pToc}</li>
+        <li class="list-group-item">Installation Details:  ${answers.iDetails}</li>
+        <li class="list-group-item">User Details:  ${answers.uDetails}</li>
+        <li class="list-group-item">License Details:  ${answers.license}</li>
+        <li class="list-group-item">How to contribute to the project:  ${answers.contribution}</li>
+        <li class="list-group-item">Project testing:  ${answers.testing}</li>
+        <li class="list-group-item">Questions about the project:  ${answers.questions}</li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`;
+  }
+  
+  async function init() {
+    console.log("hi")
+    try {
+      const answers = await promptUser();
+  
+      const html = generateHTML(answers);
+  
+      await writeFileAsync("index.html", html);
+  
+      console.log("Successfully wrote to index.html");
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+  init();
+  
